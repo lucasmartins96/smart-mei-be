88503d988952ec63b7ccb4828fab75fc7ab2e471
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2024-09-29 16:04:06 -0300

    configuração da conexão do postgres com sequelize

---
+++ src/config/db/postgres.ts
@@ -0,0 +1,39 @@
+import { Sequelize } from 'sequelize-typescript';
+import environmentVar from '../../env';
+import dbConfigs from '../../constants/db-configs';
+import SequelizeConnection from '../../interfaces/sequelize-connection';
+
+class PostgresConnection implements SequelizeConnection {
+	sequelize?: Sequelize | undefined;
+
+	constructor() {}
+
+	async connectToDatabase(): Promise<void> {
+		const { name, user, password, port, host } = environmentVar.db;
+
+		this.sequelize = new Sequelize({
+			database: name,
+			username: user,
+			password,
+			host,
+			port: Number(port),
+			dialect: 'postgres',
+			pool: {
+				acquire: dbConfigs.ACQUIRE_TIME_IN_MILLISECONDS,
+				idle: dbConfigs.IDLE_TIME_IN_MILLISECONDS,
+			},
+			models: [__dirname + '../../models'],
+		});
+
+		await this.sequelize
+			.authenticate()
+			.then(() => {
+				console.log('Conexão foi estabelecida com sucesso!');
+			})
+			.catch((err) => {
+				console.error('Não é possível conectar ao banco de dados:', err);
+			});
+	}
+}
+
+export default PostgresConnection;
