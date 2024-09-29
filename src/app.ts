c72d0dbdbc534dd0255f741a45e1e3a97ba6fcf8
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2024-09-29 16:15:34 -0300

    configuração da classes App

---
+++ src/app.ts
@@ -0,0 +1,59 @@
+import express, { Application } from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+import ErrorHandler from './common/error-handler';
+import Route from './interfaces/route';
+import PostgresConnection from './config/db/postgres';
+import SequelizeConnection from './interfaces/sequelize-connection';
+
+class App {
+	private readonly app: Application;
+	private readonly port: number;
+	private readonly routers: Route[];
+	private readonly dbConnection: SequelizeConnection;
+
+	constructor(port: number, routers: Route[]) {
+		this.app = express();
+		this.port = port;
+		this.routers = routers;
+		this.dbConnection = new PostgresConnection();
+		this.initialize();
+	}
+
+	private initializeDatabaseConnection(): void {
+		this.dbConnection.connectToDatabase();
+	}
+
+	private initializeMiddlewares(): void {
+		this.app.use(cors());
+		this.app.use(helmet());
+		this.app.use(express.json());
+		this.app.use(express.urlencoded({ extended: true }));
+	}
+
+	private initializeRoutes(): void {
+		this.routers.forEach((route) => {
+			this.app.use(`/api/v1/${route.path}`, route.router);
+		});
+	}
+
+	private initializeErrorHandling(): void {
+		this.app.use(ErrorHandler.notFound);
+		this.app.use(ErrorHandler.serverError);
+	}
+
+	private initialize(): void {
+		this.initializeDatabaseConnection();
+		this.initializeMiddlewares();
+		this.initializeRoutes();
+		this.initializeErrorHandling();
+	}
+
+	public listen(): void {
+		this.app.listen(this.port, () => {
+			console.log(`App listening on port ${this.port}`);
+		});
+	}
+}
+
+export default App;
