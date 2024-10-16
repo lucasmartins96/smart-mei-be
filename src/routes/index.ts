84596556308d7655f14f4a3599588f3b2ded312a
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2024-10-15 21:33:05 -0300

    novo roteador BusinessManRoute

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,14 @@
 import Route from '../interfaces/route';
 import AuthRoute from './auth';
+import BusinessManRoute from './business-man';
 import TestRoute from './test';
 import UserRoute from './user';
 
-const routes: Route[] = [new TestRoute(), new AuthRoute(), new UserRoute()];
+const routes: Route[] = [
+	new TestRoute(),
+	new AuthRoute(),
+	new UserRoute(),
+	new BusinessManRoute(),
+];
 
 export default routes;
