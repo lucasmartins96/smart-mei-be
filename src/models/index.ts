556e7691d33b7724c21ac3c8d123941566aab7e6
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2024-10-15 21:33:34 -0300

    novos models Address e BusinessMan

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,5 @@
+import { Address } from './address';
+import BusinessMan from './business-man';
 import { User } from './user';
 
-export default [User];
+export default [User, Address, BusinessMan];
