495ff0b9fee63b1787bce448624431a4e133a888
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2024-10-23 17:20:32 -0300

    ajustes no model CompanyActivity

--- src/models/company-activity.ts
+++ src/models/company-activity.ts
@@ -1,25 +1,28 @@
-import {
-	Column,
-	DataType,
-	ForeignKey,
-	Model,
-	// PrimaryKey,
-	Table,
-} from 'sequelize-typescript';
+import { Column, ForeignKey, Model, Table } from 'sequelize-typescript';
 import { Company } from './company';
 import { Activity } from './activity';
+import { Optional } from 'sequelize';
+
+interface CompanyActivityAttributes {
+	companyId: number;
+	activityId: number;
+}
+
+type CompanyActivityCreationAttributes = Optional<
+	CompanyActivityAttributes,
+	'companyId'
+>;
 
 @Table({ tableName: 'empresa_atividade', timestamps: false })
-export class CompanyActivity extends Model<CompanyActivity> {
+export class CompanyActivity extends Model<
+	CompanyActivityAttributes,
+	CompanyActivityCreationAttributes
+> {
 	@ForeignKey(() => Company)
-	@Column(DataType.SMALLINT)
+	@Column({ field: 'empresa_id' })
 	companyId!: number;
 
 	@ForeignKey(() => Activity)
-	@Column(DataType.SMALLINT)
+	@Column({ field: 'atividade_id' })
 	activityId!: number;
-
-	// @PrimaryKey
-	// @Column(DataType.SMALLINT)
-	// id!: number;
 }
