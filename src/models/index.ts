fdd74c8c14162a997cadbd712ef858664711d29d
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2024-10-23 17:18:11 -0300

    importando novos models

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,17 @@
+import { Account } from './account';
+import { Activity } from './activity';
 import { Address } from './address';
-import BusinessMan from './business-man';
+import { BusinessMan } from './business-man';
+import { Company } from './company';
+import { CompanyActivity } from './company-activity';
 import { User } from './user';
 
-export default [User, Address, BusinessMan];
+export default [
+	User,
+	Address,
+	BusinessMan,
+	Company,
+	Activity,
+	Account,
+	CompanyActivity,
+];
